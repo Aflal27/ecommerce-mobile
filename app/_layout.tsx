b79b39c8ab7fbbb64a1289a1bd5bf9dc2fe5cbff
Aflal <mohamedaflal6719@gmail.com>
2025-02-08 16:33:35 +0530

    auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,50 @@
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
 import { Icon } from '@/components/ui/icon'
 import '@/global.css'
+import { useAuth } from '@/store/authStore'
 import { useCart } from '@/store/cartStore'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { Link, Slot, Stack } from 'expo-router'
-import { ShoppingCart } from 'lucide-react-native'
+import { ShoppingCart, User } from 'lucide-react-native'
 import { Pressable, Text } from 'react-native'
 
 const queryClient = new QueryClient()
 
 export default function RootLayout() {
-  const cartItemsNumber = useCart((state) => state.items.length)
+  const cartItemsNum = useCart((state) => state.items.length)
+  const isLoggedIn = useAuth((s) => !!s.token)
+
   return (
     <QueryClientProvider client={queryClient}>
       <GluestackUIProvider mode='light'>
         <Stack
           screenOptions={{
-            headerRight: () => (
-              <Link href='/cart' asChild>
-                <Pressable className=' flex-row'>
-                  <Icon as={ShoppingCart} />
-                  <Text>{cartItemsNumber}</Text>
-                </Pressable>
-              </Link>
-            ),
+            headerRight: () =>
+              cartItemsNum > 0 && (
+                <Link href={'/cart'} asChild>
+                  <Pressable className='flex-row gap-2'>
+                    <Icon as={ShoppingCart} />
+                    <Text>{cartItemsNum}</Text>
+                  </Pressable>
+                </Link>
+              ),
           }}
         >
-          <Stack.Screen name='index' options={{ title: 'Shop' }} />
+          <Stack.Screen
+            name='index'
+            options={{
+              title: 'Shop',
+              headerLeft: () =>
+                !isLoggedIn && (
+                  <Link href={'/login'} asChild>
+                    <Pressable className='flex-row gap-2'>
+                      <Icon as={User} />
+                    </Pressable>
+                  </Link>
+                ),
+            }}
+          />
           <Stack.Screen name='product/[id]' options={{ title: 'Product' }} />
         </Stack>
       </GluestackUIProvider>
